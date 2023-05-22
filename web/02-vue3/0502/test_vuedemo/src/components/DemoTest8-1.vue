e299bb8bd7607045b97b42eea387d380f683d301
hunterrock <hunterrock2000@163.com>
2023-05-23 00:01:01 +0800

    0523_home_commit

---
+++ web/02-vue3/0502/test_vuedemo/src/components/DemoTest8-1.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- v-model指令 ：用于HTML元素的表单绑定;只能绑定value属性，所以value可以省略
+         -->
+        <div>
+            <p>姓名<input type="text" v-model="name"></p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+    setup () {
+        const state = reactive({
+            count: 0,
+            name:'张三'
+        })
+
+    
+        return {
+            ...toRefs(state),
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
