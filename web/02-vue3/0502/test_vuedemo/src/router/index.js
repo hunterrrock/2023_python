197f34b914f2185b1f46efd595ccb4e52949cf8a
hunterrock <hunterrock2000@163.com>
2023-06-12 16:58:28 +0800

    update .gitignore

--- web/02-vue3/0502/test_vuedemo/src/router/index.js
+++ web/02-vue3/0502/test_vuedemo/src/router/index.js
@@ -29,7 +29,7 @@ const router = createRouter(
         // this generates a separate chunk (About.[hash].js) for this route   这将为此路由生成一个单独的模块（about.【一串哈希值hash】.js)
         // which is lazy-loaded when the route is visited.    当访问路由时，是懒加载的，只有当有人访问的时候才会加载，不会事先加载
         // 总结：路由懒加载方式，项目打包时，一个组件打包成一个js文件；多少个组件就打包成多少个js文件，访问时才加载组件。
-        component: () => import('../views/AboutView.vue')
+        component: () => import('../views/login.vue')
       }
     ]
   })
