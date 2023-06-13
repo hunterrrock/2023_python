4f83a3a117002f5786f0a78f1709cca8f7d0c059
hunterrock <hunterrock2000@163.com>
2023-06-13 20:09:41 +0800

    commit_work_0613

--- web/02-vue3/0502/test_vuedemo/src/main.js
+++ web/02-vue3/0502/test_vuedemo/src/main.js
@@ -10,8 +10,20 @@ import App from './App.vue'
 import router from './router'
 import './assets/css/common.scss'
 
+// 全局配置icon图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+
+// 实例化vue对象
 const app = createApp(App)
 
+
+// 做一个for循环，遍历出来所有的图标，再用vue实例.component，都给绑定到vue实例
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+// 使用第三方插件库
 app.use(createPinia())
 app.use(router)
 
@@ -19,8 +31,8 @@ app.use(router)
 // 比如size（选项：large[大]、default[中等]、small[小]）
 // local 语言
 // zIndex:层级（提示框显示的层级，数字越大越靠前）
-const elementConfig = {size:'large',locale:zhCn,zIndex:3000}        
-app.use(ElementPlus , elementConfig)
+const elementConfig = { size: 'large', locale: zhCn, zIndex: 3000 }
+app.use(ElementPlus, elementConfig)
 
 
 app.mount('#app')
