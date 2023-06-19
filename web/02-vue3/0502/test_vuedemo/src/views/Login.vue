df926e9aad1cb5d26ef4baaa11c2b545350d151d
hunterrock <hunterrock2000@163.com>
2023-06-19 22:19:45 +0800

    commit_home_0619

--- web/02-vue3/0502/test_vuedemo/src/views/Login.vue
+++ web/02-vue3/0502/test_vuedemo/src/views/Login.vue
@@ -114,14 +114,18 @@
 
 <script>
 import { reactive, toRefs, ref } from 'vue'
-// 导入想要的图标
+// 导入想要的图标[全局配置后就不需要导入注册了]
 // <el-icon><User /></el-icon>
 // <el-icon><Platform /></el-icon>
 // <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-ea893728=""><path fill="currentColor" d="M512 160c320 0 512 352 512 352S832 864 512 864 0 512 0 512s192-352 512-352zm0 64c-225.28 0-384.128 208.064-436.8 288 52.608 79.872 211.456 288 436.8 288 225.28 0 384.128-208.064 436.8-288-52.608-79.872-211.456-288-436.8-288zm0 64a224 224 0 1 1 0 448 224 224 0 0 1 0-448zm0 64a160.192 160.192 0 0 0-160 160c0 88.192 71.744 160 160 160s160-71.808 160-160-71.744-160-160-160z"></path></svg>
-import { User,Edit } from '@element-plus/icons-vue'
+// import { User,Edit } from '@element-plus/icons-vue'
+
+// 导入消息组件
+import { ElMessage } from 'element-plus'
+
 
 export default {
-  components: { User,Edit },
+  // components: { User,Edit },
   setup() {
     // 1.定义登录时候提交用的form对象
     const loginForm = reactive({
@@ -157,6 +161,10 @@ export default {
     // 6.登录
     const onSubmit = () => {
       console.log(loginForm.username)
+        // 消息组件
+
+
+
     }
 
     return {
